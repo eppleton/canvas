734bf7da7ad4ac6221110245e6d7ed885c7e7eee
eppleton <info@eppleton.de>
2014-02-18 17:19:11 +0100

    removed dependency on lookup api for now.

--- javafx-canvas/src/main/java/de/eppleton/canvas/javafx/JavaFXGraphicsEnvironment.java
+++ javafx-canvas/src/main/java/de/eppleton/canvas/javafx/JavaFXGraphicsEnvironment.java
@@ -43,13 +43,11 @@ import net.java.html.canvas.ImageData;
 import net.java.html.canvas.Style;
 import net.java.html.canvas.spi.GraphicsEnvironment;
 import net.java.html.canvas.spi.GraphicsUtils;
-import org.openide.util.lookup.ServiceProvider;
 
 /**
  *
  * @author antonepple
  */
-@ServiceProvider(service = GraphicsEnvironment.class)
 public class JavaFXGraphicsEnvironment implements GraphicsEnvironment {
 
     GraphicsContext gc;
