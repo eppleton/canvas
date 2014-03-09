1c91faf3934cd18dd757c22ab2ffa1692682a68b
eppleton <info@eppleton.de>
2014-03-09 09:38:58 +0100

    added Factory Interface for GraphicsContext

---
+++ canvas-api/src/main/java/net/java/html/canvas/spi/GraphicsFactory.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.java.html.canvas.spi;
+
+import net.java.html.canvas.GraphicsContext2D;
+
+/**
+ *
+ * @author antonepple
+ */
+interface GraphicsFactory {
+
+    GraphicsContext2D getOrCreate(String canvasId);
+    
+}
