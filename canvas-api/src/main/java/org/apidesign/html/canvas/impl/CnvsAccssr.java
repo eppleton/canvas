e52ce73879e3a9d5895ce660bb1471564576d63e
eppleton <info@eppleton.de>
2015-01-29 18:28:29 +0100

    switching to dukescript group id, getting rid of JavaFX implementation, and removing the archetype.

--- canvas-api/src/main/java/org/apidesign/html/canvas/impl/CnvsAccssr.java
+++ canvas-api/src/main/java/org/apidesign/html/canvas/impl/CnvsAccssr.java
@@ -1,18 +1,24 @@
-/**
- * Canvas API Copyright (C) 2013 AntonEpple <toni.epple@eppleton.de>
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program. Look for COPYING file in the top folder. If not, see
- * http://opensource.org/licenses/GPL-2.0.
+/*
+ * #%L
+ * DukeScript Canvas API - a library from the "DukeScript Canvas API" project.
+ * Visit http://dukescript.com for support and commercial license.
+ * %%
+ * Copyright (C) 2015 Eppleton IT Consulting
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
  */
 package org.apidesign.html.canvas.impl;
 
--- html5-canvas/src/main/java/de/eppleton/canvas/html/HTML5Graphics.java
+++ html5-canvas/src/main/java/de/eppleton/canvas/html/HTML5Graphics.java
@@ -1,7 +1,24 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * #%L
+ * DukeScript HTML5 Canvas - a library from the "DukeScript Canvas API" project.
+ * Visit http://dukescript.com for support and commercial license.
+ * %%
+ * Copyright (C) 2015 Eppleton IT Consulting
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
  */
 package de.eppleton.canvas.html;
 
